099a7d418d020313b0e12912eb40e2d2fbce2b0a
Graham Perrin <grahamperrin@gmail.com>
2023-04-08 17:29:15 +0100

    pkg-stage.sh: DRM: modernise
    graphics/drm-legacy-kmod <https://www.freshports.org/graphics/drm-legacy-kmod/> and graphics/drm-stable-kmod <https://www.freshports.org/graphics/drm-stable-kmod/> are dead. 
    
    Long live graphics/drm-kmod <https://www.freshports.org/graphics/drm-kmod/>! 
    
    The number of indirect dependencies at <https://www.freshports.org/graphics/gpu-firmware-kmod/#dependencies> is somewhat larger than the number for former monarchs. 
    
    Re: <https://github.com/freebsd/freebsd-ports/blob/50d4c8e9a239e51fbe6c1b889d9fe0275e845525/graphics/drm-kmod/Makefile#L19-L24>, installation of the meta port will automatically install either graphics/drm-fbsd12.0-kmod or graphics/drm-510-kmod.

--- release/scripts/pkg-stage.sh
+++ release/scripts/pkg-stage.sh
@@ -15,8 +15,7 @@ export PORTSDIR="${PORTSDIR:-/usr/ports}"
 _DVD_PACKAGES="archivers/unzip
 devel/git
 emulators/linux_base-c7
-graphics/drm-legacy-kmod
-graphics/drm-stable-kmod
+graphics/drm-kmod
 misc/freebsd-doc-all
 net/mpd5
 net/rsync
